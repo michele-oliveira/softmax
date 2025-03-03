6b404ad2bd884e88dbc9b50557c874fd6f60ce25
michele-oliveira <micheleoliveiranovais24@gmail.com>
2025-03-03 19:17:23 -0300

    Removendo import não utilizado

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import ModulesPage from "./pages/ModulesPage";
-import Register from "./pages/manual/CarrierRegistration";
 import Contact from "./pages/Contact";
 import Manual from "./pages/Manual";
 
